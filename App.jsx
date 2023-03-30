dd675d6fe8ca73d2265eacedea87d681c801c4b0
Anuj Bhati <anujbhati7488@gmail.com>
2023-03-30 10:30:26 +0530

    Some changes and folder added

--- App.jsx
+++ App.jsx
@@ -1,26 +1,14 @@
-import React from 'react'
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Provider } from 'react-redux';
+import React from 'react';
+import {Provider} from 'react-redux';
 import store from './src/redux/store';
-import Main from './src/screens/Main';
-import UserInfo from './src/screens/UserInfo';
-const Stack = createNativeStackNavigator();
+import Navigation from './src/screens/Navigation';
+
 const App = () => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName='Main'
-          screenOptions={{
-            headerShown: false,
-          }}>
-          <Stack.Screen name="Main" component={Main} />
-          <Stack.Screen name="UserInfo" component={UserInfo} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Navigation />
     </Provider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/util/Colors.jsx
+++ src/util/Colors.jsx
@@ -1,12 +1,15 @@
 export const Colors = {
-  white: "#FFF",
-  black: "#000",
-  blue: "#2269f4",
+  white: '#FFF',
+  black: '#000',
+  blue: '#2269f4',
   blue2: '#82eefd',
   blueLight: '#64beff',
-  grey: "#a6a6a6",
-  darkgrey: "#808080",
-  red: "#ff0000",
-  yellow: "#fbc10d",
-  transparent: "#00000040",
+  grey: '#a6a6a6',
+  darkgrey: '#808080',
+  red: '#ff0000',
+  yellow: '#fbc10d',
+  transparent: '#00000040',
+  mainColor: '#14302e', //main
+  background: '#e5e5e5',
+  black: '#252525',
 };
