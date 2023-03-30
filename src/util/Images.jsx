42c2071fb62efec2002ff4a07b80448b2fdea90a
Anuj Bhati <anujbhati7488@gmail.com>
2023-03-31 00:26:45 +0530

    Another by anuj

--- src/util/Images.jsx
+++ src/util/Images.jsx
@@ -1,15 +1,12 @@
 const CustomImage = {
-<<<<<<< HEAD
   back: require('../assets/back.png'),
   call: require('../assets/call.png'),
   location: require('../assets/location.png'),
   search: require('../assets/search.png'),
   delete: require('../assets/delete.png'),
-=======
   back: require("../assets/back.png"),
   call: require("../assets/call.png"),
   location: require("../assets/location.png"),
   logo: require("../assets/logo.png"),
->>>>>>> dae9b296c5b4074e00ae85dd273893ea8ee38e89
 };
 export default CustomImage;
